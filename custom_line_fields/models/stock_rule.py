f21724c6dec62f4d471a2400d3899ef482345a43
mari zeng <mari@conwayamerica.com>
2026-03-25 16:42:01 -0400

    add logging to check how po is created

--- custom_line_fields/models/stock_rule.py
+++ custom_line_fields/models/stock_rule.py
@@ -1,6 +1,10 @@
+import logging
+
 from odoo import models
 
 from ._constants import LINE_FIELDS
+
+_logger = logging.getLogger(__name__)
 
 
 class StockRule(models.Model):
@@ -12,9 +16,16 @@ class StockRule(models.Model):
         vals = super()._prepare_purchase_order_line(
             product_id, product_qty, product_uom, company_id, values, po
         )
+        _logger.info(
+            'custom_line_fields._prepare_purchase_order_line: values keys = %s, '
+            'sale_line_id = %s',
+            list(values.keys()),
+            values.get('sale_line_id'),
+        )
         sale_line_id = values.get('sale_line_id')
         if sale_line_id:
             sale_line = self.env['sale.order.line'].browse(sale_line_id)
-            for f in LINE_FIELDS:
-                vals[f] = sale_line[f]
+            if sale_line.exists():
+                for f in LINE_FIELDS:
+                    vals[f] = sale_line[f]
         return vals
