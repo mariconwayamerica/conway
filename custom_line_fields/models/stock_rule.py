38e05fb9078b215899186e71e5e05f0dcf1aef85
mari zeng <mari@conwayamerica.com>
2026-03-25 15:44:00 -0400

    fix so to po logic

--- custom_line_fields/models/stock_rule.py
+++ custom_line_fields/models/stock_rule.py
@@ -12,8 +12,9 @@ class StockRule(models.Model):
         vals = super()._prepare_purchase_order_line(
             product_id, product_qty, product_uom, company_id, values, po
         )
-        sale_line = values.get('sale_line_id')
-        if sale_line:
+        sale_line_id = values.get('sale_line_id')
+        if sale_line_id:
+            sale_line = self.env['sale.order.line'].browse(sale_line_id)
             for f in LINE_FIELDS:
                 vals[f] = sale_line[f]
         return vals
