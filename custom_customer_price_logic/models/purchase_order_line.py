87113c8167eaea2012fa84f0015a1e1ac9af99bc
mari zeng <mari@conwayamerica.com>
2026-03-30 12:14:49 -0400

    change TJX percentage from placeholder to real value

--- custom_customer_price_logic/models/purchase_order_line.py
+++ custom_customer_price_logic/models/purchase_order_line.py
@@ -23,7 +23,7 @@ class PurchaseOrderLine(models.Model):
             original_customer = sale_line.order_id.x_original_customer or ''
 
             if 'TJX Companies' in original_customer:
-                vals['price_unit'] = sale_line.price_unit * 0.5
+                vals['price_unit'] = sale_line.price_unit * 0.93
                 _logger.warning(
                     'custom_customer_price_logic: TJX Companies — PO price set to SO * 0.5 = %s '
                     '(SO: %s)',
