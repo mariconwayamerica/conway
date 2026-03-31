9fe0c8694c6c3d3468ef7ced1e7c929d3f265cda
mari zeng <mari@conwayamerica.com>
2026-03-31 16:29:07 -0400

    sales orders in draft status

--- auto_confirm_orders/models/sale_order.py
+++ auto_confirm_orders/models/sale_order.py
@@ -9,6 +9,6 @@ class SaleOrder(models.Model):
         orders = super().create(vals_list)
         if not self.env.context.get('auto_confirming_sale'):
             orders.with_context(auto_confirming_sale=True).filtered(
-                lambda o: o.state == 'draft'
+                lambda o: not o._confirmation_error_message()
             ).action_confirm()
         return orders
