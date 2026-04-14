a6e9eb18967a25b55d1e4f29ccc481827f3e7916
mari zeng <mari@conwayamerica.com>
2026-04-14 17:13:36 -0400

    forgot depends lol

--- auto_send_po/__manifest__.py
+++ auto_send_po/__manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'Automatically email confirmed POs daily based on original customer',
     'category': 'Purchase',
     'author': 'Conway',
-    'depends': ['purchase', 'custom_so_fields', 'mail'],
+    'depends': ['purchase', 'custom_so_fields', 'custom_purchase_order_pdf', 'mail'],
     'data': ['data/cron.xml'],
     'installable': True,
     'application': False,
