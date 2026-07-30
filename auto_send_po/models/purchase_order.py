ae15e3addbc604a838709f368a263862b30db9d2
mari zeng <mari@conwayamerica.com>
2026-07-30 13:11:13 -0400

    add sl home fashions

--- auto_send_po/models/purchase_order.py
+++ auto_send_po/models/purchase_order.py
@@ -15,6 +15,7 @@ _CUSTOMER_EMAIL_MAP = [
     ('TJX', 'jackie.zhang@loftex.com.cn'),
     ('Costco', 'monica.cui@loftex.com.cn'),
     ('Uline', 'victor.guo@loftex.com.cn'),
+    ('SL Home', 'jackie.zhang@loftex.com.cn'),
 ]
 
 
